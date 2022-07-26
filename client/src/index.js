02c0d02436c91f5750a5c501e724c4196aeafbd1
Vijay Choudhary <scriptyvijay@gmail.com>
2022-07-26 21:04:26 +0530

    Expense tracker

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+import { store } from './store/store';
+import { Provider } from 'react-redux';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
+
